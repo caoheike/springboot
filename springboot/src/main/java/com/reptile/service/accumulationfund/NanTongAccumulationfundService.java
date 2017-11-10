c724e8bb0b0667b35cc6d4d9de77969cbf943b12
xuesongcui <m17301071346@163.com>
2017-11-10 17:51:24 +0800

    南通公积金优化、南通社保数据查询及解析

--- springboot/src/main/java/com/reptile/service/accumulationfund/NanTongAccumulationfundService.java
+++ springboot/src/main/java/com/reptile/service/accumulationfund/NanTongAccumulationfundService.java
@@ -1,24 +1,34 @@
 package com.reptile.service.accumulationfund;
 
-import com.gargoylesoftware.htmlunit.CollectingAlertHandler;
-import com.gargoylesoftware.htmlunit.Page;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebRequest;
-import com.gargoylesoftware.htmlunit.html.DomElement;
-import com.gargoylesoftware.htmlunit.html.DomNodeList;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.reptile.util.WebClientFactory;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import sun.net.www.http.HttpClient;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import com.gargoylesoftware.htmlunit.CollectingAlertHandler;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTable;
+import com.reptile.util.Dates;
+import com.reptile.util.Resttemplate;
+import com.reptile.util.WebClientFactory;
 
 @Service
 public class NanTongAccumulationfundService {
@@ -65,7 +75,7 @@ public class NanTongAccumulationfundService {
             if (pageContext.contains("个人明细查询") && pageContext.contains("个人公积金基本信息")) {
                 logger.warn("登录成功，获取到基本信息");
                 HtmlPage page2 = webClient.getPage("http://58.221.92.98:8080/searchGrye.do");
-                dataMap.put("base", page2.asXml());
+                dataMap.put("basicInfos", this.parseBaseInfo(page2));
                 Calendar instance = Calendar.getInstance();
                 SimpleDateFormat sim = new SimpleDateFormat("yyyy");
                 String format = sim.format(instance.getTime());
@@ -75,14 +85,20 @@ public class NanTongAccumulationfundService {
                 for (int i = 0; i < 3; i++) {
                     HtmlPage page1 = webClient.getPage("http://58.221.92.98:8080/searchGrmx.do?year=" + years);
                     Thread.sleep(1000);
-                    itemList.add(page1.asXml());
+                    itemList.add(page1.getElementsByTagName("table").get(0).asXml());
                     System.out.println(page1.asText());
                     years--;
                 }
-                dataMap.put("item", itemList);
+                dataMap.put("flows", this.parseFlows(itemList));
+                dataMap.put("loans", this.getLoans(webClient));
                 map.put("data", dataMap);
                 map.put("userId", idCard);
                 map.put("city", cityCode);
+                map.put("cityName", "南通");
+                map.put("insertTime", Dates.currentTime());
+                //数据推送
+//			    data = new Resttemplate().SendMessage(data,application.getSendip()+"/HSDC/person/accumulationFund");
+                map = new Resttemplate().SendMessage(map,"http://192.168.3.16:8089/HSDC/person/accumulationFund");
             } else {
                 map.put("errorCode", "0001");
                 map.put("errorInfo", "认证过程中出现未知错误");
@@ -99,4 +115,144 @@ public class NanTongAccumulationfundService {
         }
         return map;
     }
+    
+    
+    /**
+     * 获取贷款信息
+     * @param webClient
+     * @return
+     * @throws FailingHttpStatusCodeException
+     * @throws MalformedURLException
+     * @throws IOException
+     */
+    public Map<String,Object> getLoans(WebClient webClient) throws Exception{
+    	HtmlPage page = webClient.getPage("http://58.221.92.98:8080/searchGrhkmx.do");
+    	
+    	Map<String,Object> loans = new HashMap<String, Object>();
+    	System.out.println(page.asText());
+    	if(!page.asText().contains("根据您提供的身份证号码未查询到您的贷款信息")){
+    		
+    		String table = page.getElementsByTagName("table").get(0).asXml();
+    		
+    		List<List<String>> list = table1(table);
+    		loans.put("loanAccNo", "");//贷款账号
+    		loans.put("loanLimit", list.get(2).get(1));//贷款期限
+    		loans.put("openDate",   list.get(1).get(0));//开户日期
+    		loans.put("loanAmount", NanTongAccumulationfundService.numFormat(list.get(2).get(0)));//贷款总额
+    		loans.put("lastPaymentDate", list.get(4).get(0));//最近还款日期
+    		loans.put("status",  "");//还款状态
+    		loans.put("loanBalance", NanTongAccumulationfundService.numFormat(list.get(3).get(1)));//贷款余额
+    		loans.put("paymentMethod", list.get(5).get(0));//还款方式
+    	}
+    	
+    	return loans;
+    	
+    }
+    
+    
+    /**
+	 * 解析基础信息
+	 * @param xml
+	 * @return
+     * @throws ParseException 
+	 */
+	public Map<String,Object> parseBaseInfo(HtmlPage loadPage) throws ParseException{
+		HtmlTable table = (HtmlTable) loadPage.getElementsByTagName("table").get(0);
+		
+		List<List<String>> list = table1(table.asXml());
+		
+		Map<String,Object> baseInfo = new HashMap<String, Object>();
+		baseInfo.put("name",list.get(1).get(0));//用户姓名
+		baseInfo.put("idCard",list.get(1).get(1));//身份证号码
+		baseInfo.put("companyFundAccount",list.get(0).get(1));//单位公积金账号
+		baseInfo.put("personFundAccount",list.get(2).get(0));//个人公积金账号
+		baseInfo.put("companyName",list.get(0).get(0));//公司名称
+		baseInfo.put("personFundCard","");//个人公积金卡号
+		baseInfo.put("baseDeposit",NanTongAccumulationfundService.numFormat(list.get(2).get(1)));//缴费基数
+		baseInfo.put("companyRatio","");//公司缴费比例
+		baseInfo.put("personRatio","");//个人缴费比例
+		baseInfo.put("personDepositAmount","");//个人缴费金额
+		baseInfo.put("companyDepositAmount","");//公司缴费金额
+		baseInfo.put("lastDepositDate",list.get(5).get(0));//最后缴费日期
+		baseInfo.put("balance",NanTongAccumulationfundService.numFormat(list.get(3).get(1)));//余额
+		baseInfo.put("status",list.get(5).get(1));//状态（正常）
+		
+		return baseInfo;
+	}
+	
+	/**
+	 * 解析公积金交易流水信息
+	 * @param xml
+	 * @param company
+	 * @return
+	 * @throws ParseException 
+	 */
+	public List<Map<String,Object>> parseFlows(List<String> itemList) throws ParseException{
+		List<Map<String,Object>> flows = new ArrayList<Map<String,Object>>();
+		for (int i = 0; i < itemList.size(); i++) {
+			
+			List<List<String>> list = table1(itemList.get(i));
+			
+			for (int j = 1; j < list.size(); j++) {
+				List<String> trItem = list.get(j); 
+				if(trItem.get(3).contains("汇缴") || trItem.get(3).contains("补缴")){
+					Map<String,Object> item = new HashMap<String, Object>();
+					item.put("operatorDate",trItem.get(1));//操作时间（2015-08-19）
+					item.put("amount",NanTongAccumulationfundService.numFormat(trItem.get(4)));//操作金额
+					String type = "";
+					if(trItem.get(3).contains("汇缴")){
+						type = "汇缴";
+					}else if(trItem.get(3).contains("补缴")){
+						type = "补缴";
+					}
+					item.put("type",type);//操作类型
+					item.put("bizDesc",type + trItem.get(2)+"公积金");//业务描述（汇缴201508公积金）
+					item.put("companyName","");//单位名称
+					item.put("payMonth",trItem.get(2));//缴费月份
+					flows.add(item);
+				}
+				
+			}
+		}
+		
+		return flows;
+	}
+    
+    
+	
+	/**
+	 * 解析table
+	 * @param xml
+	 * @return
+	 */
+	 private static List<List<String>>  table1(String xml){ 
+		 
+		 Document doc = Jsoup.parse(xml);
+		 Elements trs = doc.select("table").select("tr");  
+		 
+		 List<List<String>> list = new ArrayList<List<String>>();
+		 for (int i = 0; i < trs.size(); i++) {
+			 Elements tds = trs.get(i).select("td");
+			 List<String> item = new ArrayList<String>();
+			 for (int j = 0; j < tds.size(); j++){
+				 String txt = tds.get(j).text().replace(" ", "").replace(" ", "").replace("元", "");
+				 item.add(txt);
+			 }  
+			 list.add(item);
+		 }
+		 
+		return list;	
+    } 
+	 
+	 /**
+	  * 去除千分符
+	  * @param amount
+	  * @return
+	  * @throws ParseException
+	  */
+	 private static String numFormat(String amount) throws ParseException{
+		 return amount.replace(",", "");
+	 }
+	 
+    
 }
