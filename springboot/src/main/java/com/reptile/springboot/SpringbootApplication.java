244222320a7c86e7a5475cc71005cd0954a169c9
ludangwei <100900386@qq.com>
2017-11-28 16:02:14 +0800

    项目改用外置的tomcat mrlu 1128

--- springboot/src/main/java/com/reptile/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/reptile/springboot/SpringbootApplication.java
@@ -3,25 +3,23 @@ package com.reptile.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
 @ComponentScan(basePackages={"com.reptile"})
 @SpringBootApplication
 @EnableScheduling
-public class SpringbootApplication extends WebMvcConfigurerAdapter {
-	 @Override
-     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-          configurer.favorPathExtension(false);
-      }
+public class SpringbootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
