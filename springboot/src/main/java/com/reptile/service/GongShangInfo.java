431d0cb929c05461d98a28209e17614d9ddddbde
ludangwei <100900386@qq.com>
2017-12-20 14:02:17 +0800

    处理base64无法正常打包  mrlu 12 20

--- springboot/src/main/java/com/reptile/service/GongShangInfo.java
+++ springboot/src/main/java/com/reptile/service/GongShangInfo.java
@@ -1,6 +1,7 @@
 package com.reptile.service;
 
 import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.TextPage;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.DomNodeList;
@@ -14,16 +15,10 @@ public class GongShangInfo {
     public static void main(String[] args) throws IOException {
         WebClient webClient = new WebClientFactory().getWebClient();
         HtmlPage page = webClient.getPage("http://sn.gsxt.gov.cn");
-        DomNodeList<DomElement> input = page.getElementsByTagName("input");
-        for(int i=0;i<input.size();i++){
-            NamedNodeMap attributes = input.get(i).getAttributes();
-            for(int j=0;j<attributes.getLength();j++){
-                String textContent = attributes.item(j).getTextContent();
-                if(textContent.contains("请输入企业名称、统一社会信用代码或注册号")){
-                    input.get(i).setAttribute("value","");
-                }
-            }
-        }
+
+        TextPage page1 = webClient.getPage("http://sn.gsxt.gov.cn/StartCaptchaServlet?&time=" + System.currentTimeMillis());
+        System.out.println(page1.getContent());
+
     }
 
 }
