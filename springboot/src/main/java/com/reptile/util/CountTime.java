c102b43581a3ec84842e030e63bab8c3bfdd0009
fangshuang <971147928@qq.com>
2017-12-29 15:37:42 +0800

    countTime类阿里检测

--- springboot/src/main/java/com/reptile/util/CountTime.java
+++ springboot/src/main/java/com/reptile/util/CountTime.java
@@ -4,7 +4,14 @@ package com.reptile.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+/**
+ * 信用卡判断是否推送100，200，300
+ * @ClassName: CountTime  
+ * @Description: TODO  
+ * @author: fangshuang
+ * @date 2017年12月29日  
+ *
+ */
 public class CountTime {
 	public static boolean getCountTime(String timeCnt) throws ParseException {
 		Date date = new Date();
@@ -13,7 +20,8 @@ public class CountTime {
 		Date newDate = sdf.parse(newTime);
 		Date cntDate = sdf.parse(timeCnt);
 		int days = (int) ((newDate.getTime()-cntDate.getTime())/(1000*60*60*24));
-		if(days>30) {
+		final int countday = 30;
+		if(days>countday) {
 			return true;
 		}else {
 			return false;
