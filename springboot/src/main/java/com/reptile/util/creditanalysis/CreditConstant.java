3281d46982d2a34c0eecbd8ae9a285dea0df52b5
ludangwei <100900386@qq.com>
2018-01-04 11:07:56 +0800

    mrludw 征信解析异常处理 18 1 3

---
+++ springboot/src/main/java/com/reptile/util/creditanalysis/CreditConstant.java
@@ -0,0 +1,24 @@
+package com.reptile.util.creditanalysis;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreditConstant {
+    public static Map<String,String> successMap=new HashMap<>();
+
+    static{
+        successMap.put("ResultInfo","查询成功");
+        successMap.put("ResultCode","0000");
+        successMap.put("errorInfo","查询成功");
+        successMap.put("errorCode","0000");
+    }
+
+    public static Map<String,String> setErrorFailMap(String errorcode,String errorInfo){
+        Map<String,String> failMap=new HashMap<>();
+        failMap.put("ResultInfo",errorInfo);
+        failMap.put("ResultCode",errorcode);
+        failMap.put("errorInfo",errorInfo);
+        failMap.put("errorCode",errorcode);
+        return failMap;
+    }
+}
