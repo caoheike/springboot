15fb526ad10de6bca9f780477f65b0eda5086787
fangshuang <971147928@qq.com>
2018-01-05 18:42:02 +0800

    认证失败原因格式修改

--- springboot/src/main/java/com/reptile/util/PushState.java
+++ springboot/src/main/java/com/reptile/util/PushState.java
@@ -25,7 +25,7 @@ public class PushState {
      * @param message
      */
     public static void state(String UserCard, String approveName, int stat,String message) {
-    	if("callLog".equals(approveName)) {//电信
+    	if("callLog".equals(approveName)) {
     		message = "您提交的运营商认证失败，失败原因："+message+"，您可以重新认证或者选择其他产品。";
     	}else if("CHSI".equals(approveName)){
     		message = "您提交的学信网认证失败，失败原因："+message+"，您可以重新认证或者选择其他产品。";
