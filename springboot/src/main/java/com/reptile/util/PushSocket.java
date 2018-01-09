966aabbdbfeb0903f68b4fa325b8a8efc5ab51a6
ludangwei <100900386@qq.com>
2018-01-09 18:12:32 +0800

    推送状态增加logger日志 ,以确保下次系统出现问题时，快速定位原因  mrlu 2018 19

--- springboot/src/main/java/com/reptile/util/PushSocket.java
+++ springboot/src/main/java/com/reptile/util/PushSocket.java
@@ -25,9 +25,6 @@ public class PushSocket {
 		System.out.println(se);
 		System.out.println(seq_id);
 		try {
-//			mapData.put("resultCode", errorInfo);
-//			mapData.put("seq_id", seq_id);
-//			JSONObject json=JSONObject.fromObject(mapData);
 			if(se!=null&&seq_id!=null){
 				if(seq_id.equals("hello")){
 					se.getBasicRemote().sendText("{\"resultCode\":\""+errorInfo+"\",\"seq_id\":\""+seq_id+"\"}");
