d5270546da0bc9c1d0d567150a5d43b5bf002e65
ludangwei <100900386@qq.com>
2018-01-25 11:29:00 +0800

    新增电信解析统一接口 mrlu 1 25

---
+++ springboot/src/main/java/com/reptile/analysis/ChinaTelecomAnalysisInterface.java
@@ -0,0 +1,30 @@
+package com.reptile.analysis;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ChinaTelecomAnalysisInterface {
+    /**
+     * 解析xml形式的数据
+     * @param data
+     * @param agrs
+     * @return
+     */
+    public Map<String, Object> analysisXml(List<String> data,String...agrs);
+
+    /**
+     * 解析json形式的数据
+     * @param data
+     * @param agrs
+     * @return
+     */
+    public Map<String, Object> analysisJson(List<String> data,String...agrs);
+
+    /**
+     * 解析Html形式的数据
+     * @param data
+     * @param agrs
+     * @return
+     */
+    public Map<String, Object> analysisHtml(List<String> data,String...agrs);
+}
