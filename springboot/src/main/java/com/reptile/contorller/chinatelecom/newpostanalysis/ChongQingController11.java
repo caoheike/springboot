3156e15bda4fe601690bd327634562d4b392eaed
duweiwei <3401470547@qq.com>
2018-01-30 14:27:08 +0800

    重庆电信 发包  杜维维  2018 1 30

--- springboot/src/main/java/com/reptile/contorller/chinatelecom/newpostanalysis/ChongQingController11.java
+++ springboot/src/main/java/com/reptile/contorller/chinatelecom/newpostanalysis/ChongQingController11.java
@@ -18,11 +18,11 @@ import com.reptile.util.CustomAnnotation;
 
 /**
  * 
- * @ClassName: ChongQingTelecomController
- * @Description: TODO
- * @author: 111
- * @date 2018年1月2日
- *
+* @ClassName: ChongQingController11 
+* @Description: TODO(重庆电信) 
+* @author duwei
+* @date 2018年1月30日 下午2:14:25 
+*
  */
 @Controller
 @RequestMapping("ChongQingController11")
@@ -41,7 +41,7 @@ public class ChongQingController11 {
 	@ApiOperation(value = "2.获取详单", notes = "参数：手机号，服务密码，验证码，经度，纬度，姓名，身份证后六位")
 	@ResponseBody
 	@CustomAnnotation
-	@RequestMapping(value = "CQGetDetail1", method = RequestMethod.POST)
+	@RequestMapping(value = "getDetail", method = RequestMethod.POST)
 	public Map<String, Object> getDetail(HttpServletRequest request, @RequestParam("phoneNumber") String phoneNumber,@RequestParam("passWord") String passWord,
 			@RequestParam("code") String code,
 			@RequestParam("longitude") String longitude, @RequestParam("latitude") String latitude,
